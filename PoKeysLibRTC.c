fa366d39ba9b2576f35f64315b67a7c4709e8872
dominik.zarfl <dominik.zarfl@avl.com>
2025-05-04 22:30:34 +0200

    PK_RTCSet

--- PoKeysLibRTC.c
+++ PoKeysLibRTC.c
@@ -48,16 +48,16 @@ int32_t PK_RTCSet(sPoKeysDevice* device)
     if (device == NULL) return PK_ERR_NOT_CONNECTED;
 
     CreateRequest(device->request, 0x83, 0x10, 0, 0, 0);
-    device->request[8] = *device->RTC.SEC;
-    device->request[9] = *device->RTC.MIN;
-    device->request[10] = *device->RTC.HOUR;
-    device->request[11] = *device->RTC.DOW;
-    device->request[12] = *device->RTC.DOM;
-    device->request[13] = (unsigned char)device->RTC.MONTH;
-    device->request[14] = device->RTC.DOY & 0xFF;
-    device->request[15] = (device->RTC.DOY >> 8) & 0xFF;
-    device->request[16] = *device->RTC.YEAR & 0xFF;
-    device->request[17] = (*device->RTC.YEAR >> 8) & 0xFF;
+    device->request[8]  = (uint8_t)(*device->RTC.SEC);
+    device->request[9]  = (uint8_t)(*device->RTC.MIN);
+    device->request[10] = (uint8_t)(*device->RTC.HOUR);
+    device->request[11] = (uint8_t)(*device->RTC.DOW);
+    device->request[12] = (uint8_t)(*device->RTC.DOM);
+    device->request[13] = (uint8_t)(*device->RTC.MONTH);
+    device->request[14] = (uint8_t)(*device->RTC.DOY & 0xFF);
+    device->request[15] = (uint8_t)((*device->RTC.DOY >> 8) & 0xFF);
+    device->request[16] = (uint8_t)(*device->RTC.YEAR & 0xFF);
+    device->request[17] = (uint8_t)((*device->RTC.YEAR >> 8) & 0xFF);
     if (SendRequest(device) != PK_OK) return PK_ERR_TRANSFER;
 
     return PK_OK;
