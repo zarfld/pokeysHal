bf71d64a416d9b254ae6066dd5a968e3128cf3b1
Dominik Zarfl <13350047+zarfld@users.noreply.github.com>
2025-06-16 12:19:32 +0200

    Update PoKeysLibDevicePoKeys57Industrial.c

--- PoKeysLibDevicePoKeys57Industrial.c
+++ PoKeysLibDevicePoKeys57Industrial.c
@@ -35,7 +35,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
     #include <net/if.h>
     #include <sys/ioctl.h>
 #endif
-#include "PoKeysLibDevicePoKeys57Industrial.h"
+//#include "PoKeysLibDevicePoKeys57Industrial.h"
 #include "PoKeysLibCoreSockets.h"
 #include "string.h"
 #include "stdio.h"
